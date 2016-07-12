85174f341797bc91face57e175403119dabb32d8
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-12 14:57:55 -0600

    added more HTML tags

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<main>
 
 			<p>
-				Unidentified <strong> nuclear wessels</strong> travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
+				Unidentified <strong> nuclear wessels</strong> travelling at sub warp speed, bearing 235.73. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
 			</p>
 			<ul>
 				<li>item 1</li>
