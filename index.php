d227e6bf56984e04f53e1185eaf8434f82269798
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-12 14:57:14 -0600

    added more HTML tags

--- index.php
+++ index.php
@@ -5,26 +5,53 @@
 		<title>:) My Basic HTML Page :) </title>
 	</head>
 	<body>
-		<h1>Hello, World! :D</h1>
+		<header>
+			<h1>Hello, World! :D</h1>
+		</header>
+
 		<h3>** Make it so **</h3>
-		<p>
-			Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
-		</p>
-		<ul>
-			<li>item 1</li>
-			<li>item 2</li>
-			<li>item 3</li>
-			<li>item 4</li>
-		</ul>
-		<ol>
-			<li>item 1</li>
-			<li>item 2</li>
-			<li>item 3</li>
-			<li>item 4</li>
-		</ol>
-		<div>
-			 <a href="https://cracked.com" target="_blank">go to cracked.com</a>
-		</di                               ;;.v>
+
+		<main>
+
+			<p>
+				Unidentified <strong> nuclear wessels</strong> travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
+			</p>
+			<ul>
+				<li>item 1</li>
+				<li>item 2</li>
+				<li>item 3</li>
+				<li>item 4</li>
+			</ul>
+			<ol>
+				<li>item 1</li>
+				<li>item 2</li>
+				<li>item 3</li>
+				<li>item 4</li>
+			</ol>
+			<div>
+				<a href="https://cracked.com" target="_blank">go to cracked.com</a>
+			</div>
+			<img src="images/547462_v1.jpg" alt="times square" />
+			<table>
+				<tr>
+					<td>
+						table cell 1
+					</td>
+					<td>table cell 2
+					</td>
+				</tr>
+				<tr>
+					<td>table cell 3</td>
+					<td>table cell 4</td>
+				</tr>
+			</table>
+		</main>
+
+		
+		<footer>
+			&copy; 2016
+		</footer>
 
 	</body>
 </html>
+
