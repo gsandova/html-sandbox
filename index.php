795f4549add80ded00766d5da118e6985162ad1c
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-12 12:52:26 -0600

    added lists and other tags

--- index.php
+++ index.php
@@ -6,5 +6,25 @@
 	</head>
 	<body>
 		<h1>Hello, World! :D</h1>
+		<h3>** Make it so **</h3>
+		<p>
+			Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
+		</p>
+		<ul>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+			<li>item 4</li>
+		</ul>
+		<ol>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+			<li>item 4</li>
+		</ol>
+		<div>
+			 <a href="https://cracked.com" target="_blank">go to cracked.com</a>
+		</di                               ;;.v>
+
 	</body>
 </html>
